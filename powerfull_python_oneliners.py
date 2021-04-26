e496f7378306ab8fdbd60cdaf275b43e16d8d4a6
nithyanandham-thiru-pydev <79956461+nithyanandham-thiru-pydev@users.noreply.github.com>
2021-04-26 19:44:54 +0530

    Add files via upload

---
+++ powerfull_python_oneliners.py
@@ -0,0 +1,22 @@
+# palindrome python one-liner
+
+phrase.find(phrase[::-1])
+
+
+# swap two variable python one-linear
+
+a,b = b,a
+
+
+# sum over every other value python one-linear
+
+sum (stock_prices[::2])
+
+# read over every value python one-linear
+
+[line.strip() for line in open(filename)]
+
+# factorial python one-linear
+
+reduce(lambda x,y :x*y,range(1,n+1))
+
